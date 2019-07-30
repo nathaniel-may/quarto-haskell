92b6b8fb11bd22a6d591b643c4da82fdd368c3ec
Nathaniel May <nathanieldmay@gmail.com>
2019-07-29 21:59:37 -0400

    using sumtypes correctly

--- src/lib/Quarto.hs
+++ src/lib/Quarto.hs
@@ -1,2 +1,11 @@
 module Quarto where
 
+
+data Color  = Black | White
+data Shape  = Round | Square
+data Height = Tall  | Short
+data Top    = Flat  | Hole
+
+data Piece = Color Shape Height Top
+
+data Index = I1 | I2 | I3 | I4
