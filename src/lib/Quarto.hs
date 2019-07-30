03e56340001bd86353b40fea57d29e9a6f642daf
Nathaniel May <nathanieldmay@gmail.com>
2019-07-30 12:48:06 -0400

    added board fns

--- src/lib/Quarto.hs
+++ src/lib/Quarto.hs
@@ -1,11 +1,40 @@
 module Quarto where
 
+import Data.Map (Map)
+import qualified Data.Map as Map
 
-data Color  = Black | White
-data Shape  = Round | Square
-data Height = Tall  | Short
-data Top    = Flat  | Hole
+data Color  = Black | White  deriving (Eq, Ord, Show, Read)
+data Shape  = Round | Square deriving (Eq, Ord, Show, Read)
+data Height = Tall  | Short  deriving (Eq, Ord, Show, Read)
+data Top    = Flat  | Hole   deriving (Eq, Ord, Show, Read)
 
-data Piece = Color Shape Height Top
+data Piece = Piece Color Shape Height Top deriving (Eq, Ord, Show, Read)
 
-data Index = I1 | I2 | I3 | I4
+data Index = I1 | I2 | I3 | I4 deriving (Eq, Ord, Enum, Bounded, Show, Read)
+
+indexes :: [Index]
+indexes = [minBound..maxBound]
+
+data Tile = Tile Index Index deriving (Eq, Ord, Show, Read)
+
+-- TODO make nonempty --
+newtype Board = Board { tiles :: Map Tile Piece }
+              deriving (Eq, Ord, Show, Read)
+
+isFull :: Board -> Bool
+isFull b = size b >= 16
+
+-- TODO int between 0 and 16 --
+size :: Board -> Int
+size = length . tiles
+
+contains :: Board -> Tile -> Bool
+contains b = not . null . get b
+
+-- TODO can I get rid of the maybe with LH? --
+get :: Board -> Tile -> Maybe Piece
+get b t = Map.lookup t $ tiles b
+
+-- TODO how many restrictions can I place here with LH? --
+place :: Board -> Tile -> Piece -> Maybe Board
+place = undefined
