2d0d5f65d84785bf8b05709c5c1894631788050e
Nathaniel May <nathanieldmay@gmail.com>
2019-07-31 10:35:11 -0400

    added pass fn

--- src/lib/Quarto.hs
+++ src/lib/Quarto.hs
@@ -33,13 +33,16 @@ size = length . tiles
 contains :: Board -> Tile -> Bool
 contains b = not . null . get b
 
+containsPiece :: Board -> Piece -> Bool
+containsPiece b p = elem p $ tiles b
+
 -- TODO can I get rid of the maybe with LH? --
 get :: Board -> Tile -> Maybe Piece
 get b t = Map.lookup t $ tiles b
 
 -- TODO how many restrictions can I place here with LH? --
 place :: Board -> Tile -> Piece -> Maybe Board
-place b t p = if not (contains b t) && notElem p (tiles b)
+place b t p = if not (contains b t) && not (containsPiece b p)
               then Just . Board . Map.insert t p $ tiles b
               else Nothing
 
@@ -48,9 +51,9 @@ data Player = P1 | P2 deriving (Eq, Ord, Enum, Bounded, Show, Read)
 data GameEnd = Winner Player | Tie deriving (Eq, Show, Read)
 
 -- TODO smart constructors --
-data PassQuarto  = PassQuarto  Board Piece   deriving (Eq, Show, Read)
-data PlaceQuarto = PlaceQuarto Board Piece   deriving (Eq, Show, Read)
-data FinalQuarto = FinalQuarto Board GameEnd deriving (Eq, Show, Read)
+newtype PassQuarto  = PassQuarto  Board         deriving (Eq, Show, Read)
+data    PlaceQuarto = PlaceQuarto Board Piece   deriving (Eq, Show, Read)
+data    FinalQuarto = FinalQuarto Board GameEnd deriving (Eq, Show, Read)
 
 data Quarto = Pass PassQuarto | Place PlaceQuarto | Final FinalQuarto
 
@@ -59,8 +62,15 @@ isEven b = size b `mod` 2 == 0
 
 turn :: Quarto -> Maybe Player
 turn q = case q of
-  Pass  (PassQuarto  b _) -> if isEven b then Just P1 else Just P2
+  Pass  (PassQuarto  b  ) -> if isEven b then Just P1 else Just P2
   Place (PlaceQuarto b _) -> if isEven b then Just P2 else Just P1
   Final _                 -> Nothing
+
+pass :: PassQuarto -> Player -> Piece -> Maybe PlaceQuarto
+pass q pl p = case q of
+  PassQuarto b -> (==pl) <$> turn (Pass q) >>= (\turn ->
+    if turn && not(containsPiece b p)
+    then Just $ PlaceQuarto b p
+    else Nothing)
 
 
