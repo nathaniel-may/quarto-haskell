f1a5e72dca32173b47619679ec1772ac210b25e6
Nathaniel May <nathanieldmay@gmail.com>
2019-08-01 22:47:37 -0400

    added win detection

--- src/lib/Board.hs
+++ src/lib/Board.hs
@@ -2,22 +2,45 @@ module Board where
 
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Data.Maybe (Maybe)
+import Data.Maybe (Maybe, catMaybes)
 import qualified Data.Maybe as Maybe
+import qualified Data.List as List
 
 data Color  = Black | White  deriving (Eq, Ord, Show, Read)
 data Shape  = Round | Square deriving (Eq, Ord, Show, Read)
 data Height = Tall  | Short  deriving (Eq, Ord, Show, Read)
 data Top    = Flat  | Hole   deriving (Eq, Ord, Show, Read)
 
-data Attribute = C Color
-               | S Shape
-               | H Height
-               | T Top
+data Attribute = W | B | R | Q | S | T | F | H
+               deriving (Eq, Show, Read)
 
-data Piece = Piece Color Shape Height Top deriving (Eq, Ord, Show, Read)
+data Property = PropColor Color
+              | PropShape Shape
+              | PropHeight Height
+              | PropTop Top
 
-data Index = I1 | I2 | I3 | I4 deriving (Eq, Ord, Enum, Bounded, Show, Read)
+attr :: Property -> Attribute
+attr p = case p of
+  PropColor  White  -> W
+  PropColor  Black  -> B
+  PropShape  Round  -> R
+  PropShape  Square -> Q
+  PropHeight Tall   -> T
+  PropHeight Short  -> S
+  PropTop    Flat   -> F
+  PropTop    Hole   -> H
+
+data Piece = Piece Color Shape Height Top
+           deriving (Eq, Ord, Show, Read)
+
+attrs :: Piece -> [Attribute]
+attrs (Piece c s h t) = [attr $ PropColor c,
+                         attr $ PropShape s,
+                         attr $ PropHeight h,
+                         attr $ PropTop t]
+
+data Index = I1 | I2 | I3 | I4
+           deriving (Eq, Ord, Enum, Bounded, Show, Read)
 
 indexes :: [Index]
 indexes = [minBound..maxBound]
@@ -54,7 +77,10 @@ place b t p = if not (contains b t) && not (containsPiece b p)
 isEven :: Board -> Bool
 isEven b = size b `mod` 2 == 0
 
-data Line = Horizontal Index | Vertical Index | DiagonalForward | DiagonalBackward
+data Line = Horizontal Index
+          | Vertical Index
+          | DiagonalForward
+          | DiagonalBackward
           deriving (Eq, Show, Read)
 
 data WinningLine = WinningLine Line Attribute
@@ -71,5 +97,24 @@ lineTiles (Horizontal i)   =      Tile i <$> indexes
 lineTiles DiagonalForward  = uncurry Tile <$> indexes `zip` reverse indexes
 lineTiles DiagonalBackward = uncurry Tile <$> reverse indexes `zip` indexes
 
-winningLines :: Board -> [(Line, Attribute)]
-winningLines = undefined
+same :: Eq a => [a] -> [a] -> [a]
+same (x:xs) ys = if x `elem` ys
+                 then x : same xs (List.delete x ys)
+                 else same xs ys
+same _ _ = []
+
+isWin :: Board -> Line -> [WinningLine]
+-- isWin = undefined
+isWin b line =
+  fmap (WinningLine line) .
+  foldr same [] .
+  filter (\x -> 4 == length x) .
+  catMaybes $
+  fmap attrs .
+  get b <$> lineTiles line
+
+winningLines :: Board -> [WinningLine]
+winningLines b = isWin b =<< Board.lines
+
+test :: [Int] -> [Int]
+test xs = (+3) . (+2) <$> xs
