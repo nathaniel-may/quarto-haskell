f29fb707d95950005508c65e1e6f565f435b5b81
Nathaniel May <nathanieldmay@gmail.com>
2019-08-06 12:38:54 -0400

    added third and uncurry4. exporting everything

--- src/lib/Lib.hs
+++ src/lib/Lib.hs
@@ -1,4 +1,4 @@
-module Lib (same) where
+module Lib where
 
 import Data.List (delete)
 
@@ -8,3 +8,9 @@ same _ [] = []
 same (x:xs) ys = if x `elem` ys
                  then x : same xs (delete x ys)
                  else same xs ys
+
+third :: (a,b,c) -> c
+third (_, _, c) = c
+
+uncurry4 :: (a -> b -> c -> d -> e) -> ((a, b, c, d) -> e)
+uncurry4 f ~(a,b,c,d) = f a b c d
