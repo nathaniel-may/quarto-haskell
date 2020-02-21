a1dbcc583d57c98d2ee06c6eece001172ffdf9b8
Nathaniel May <nate.may@fauna.com>
2020-02-21 13:15:41 -0500

    removed unused reading function

--- src/exe/Main.hs
+++ src/exe/Main.hs
@@ -42,27 +42,6 @@ draw (Piece c s h t) = color c (height h (shape s (top t))) where
     top    Flat     = " "
     top    Hole     = stylize ('\9675' : "")
 
--- this is a naive and error prone implementation, but it's going to go away
--- eventually these will be keyed in by numbers
-readPiece :: String -> Maybe Piece
-readPiece ['W', 'Q', 'S', 'H'] = Just $ Piece White Square Short Hole
-readPiece ['W', 'Q', 'S', 'F'] = Just $ Piece White Square Short Flat
-readPiece ['W', 'Q', 'T', 'H'] = Just $ Piece White Square Tall  Hole
-readPiece ['W', 'Q', 'T', 'F'] = Just $ Piece White Square Tall  Flat
-readPiece ['W', 'R', 'S', 'H'] = Just $ Piece White Round  Short Hole
-readPiece ['W', 'R', 'S', 'F'] = Just $ Piece White Round  Short Flat
-readPiece ['W', 'R', 'T', 'H'] = Just $ Piece White Round  Tall  Hole
-readPiece ['W', 'R', 'T', 'F'] = Just $ Piece White Round  Tall  Flat
-readPiece ['B', 'Q', 'S', 'H'] = Just $ Piece Black Square Short Hole
-readPiece ['B', 'Q', 'S', 'F'] = Just $ Piece Black Square Short Flat
-readPiece ['B', 'Q', 'T', 'H'] = Just $ Piece Black Square Tall  Hole
-readPiece ['B', 'Q', 'T', 'F'] = Just $ Piece Black Square Tall  Flat
-readPiece ['B', 'R', 'S', 'H'] = Just $ Piece Black Round  Short Hole
-readPiece ['B', 'R', 'S', 'F'] = Just $ Piece Black Round  Short Flat
-readPiece ['B', 'R', 'T', 'H'] = Just $ Piece Black Round  Tall  Hole
-readPiece ['B', 'R', 'T', 'F'] = Just $ Piece Black Round  Tall  Flat
-readPiece _ = Nothing
-
 allPieces :: [Piece]
 allPieces = [
    Piece White Square Short Hole
