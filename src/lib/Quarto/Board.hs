17a84dc3579379469f605f445ce538ac02c81c22
Nathaniel May <nate.may@fauna.com>
2020-03-11 16:50:49 -0400

    added doc strings to Board module

--- src/lib/Quarto/Board.hs
+++ src/lib/Quarto/Board.hs
@@ -6,10 +6,11 @@ import Data.Maybe (Maybe)
 import Quarto.Types.Internal
 import Quarto.Lib
 
-
+-- |An empty board.
 empty :: Board
 empty = MkBoard Map.empty
 
+-- |Isomorphism from Property to Attribute
 attr :: Property -> Attribute
 attr p = case p of
   PropColor  White  -> W
@@ -21,37 +22,45 @@ attr p = case p of
   PropTop    Flat   -> F
   PropTop    Hole   -> H
 
+-- |Extracts the attributes from a piece
 attrs :: Piece -> [Attribute]
-attrs (Piece c s h t) = [attr $ PropColor c,
-                         attr $ PropShape s,
-                         attr $ PropHeight h,
-                         attr $ PropTop t]
+attrs (Piece c s h t) = [attr (PropColor c),
+                         attr (PropShape s),
+                         attr (PropHeight h),
+                         attr (PropTop t)]
 
+-- |List of all possible pieces
 allPieces :: [Piece]
 allPieces = [ Piece c s h t | c <- enumerate
                             , s <- enumerate
                             , h <- enumerate
                             , t <- enumerate ]
 
+-- |List of all possible tiles
 allTiles :: [Tile]
 allTiles = [ Tile h v | h <- enumerate, v <- enumerate ]
 
+-- |Determins if a given board has any empty tiles or not
 full :: Board -> Bool
 full b = size b >= 16
 
--- TODO int between 0 and 16 --
+-- |The size of the board is the number of pieces placed on it
 size :: Board -> Int
 size = length . tiles
 
+-- |Determines if a 'Tile' is occupied on the given 'Board'
 contains :: Tile -> Board -> Bool
 contains t = not . null . get t
 
+-- |Determines if a 'Piece' is occupied on the given 'Board'
 containsPiece :: Piece -> Board -> Bool
 containsPiece p b = p `elem` tiles b
 
+-- |Looks up the piece that is placed on the tile if there is one
 get :: Tile -> Board -> Maybe Piece
 get t b = Map.lookup t (tiles b)
 
+-- |Places a 'Piece' on the specified 'Tile' on the 'Board'
 place :: Tile -> Piece -> Board -> Either QuartoException Board
 place t p b
   | contains t b       = Left TileOccupied
@@ -59,5 +68,6 @@ place t p b
   | containsPiece p b  = Left PieceAlreadyPlaced
   | otherwise          = board $ Map.insert t p (tiles b)
 
+-- |Determines if the size of the board is even or not
 even :: Board -> Bool
 even = Prelude.even . size
